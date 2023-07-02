0647d5c76e92d93fb3e4ebf4af16f72ca1ec7854
Scott-fo <scottfo@live.com>
2023-07-02 22:22:07 +0100

    feat: dfs / preorder

--- kata/Day1/binary_tree_depth_first.h
+++ kata/Day1/binary_tree_depth_first.h
@@ -7,6 +7,34 @@
 
 using std::vector;
 
-inline vector<int> depth_first(BinaryNode<int> *head) {}
+inline vector<int> depth_first(BinaryNode<int> *head) {
+  vector<int> result;
+
+  vector<BinaryNode<int> *> stack;
+
+  if (head == nullptr) {
+    return result;
+  }
+
+  stack.push_back(head);
+
+  do {
+    BinaryNode<int> *node = stack.back();
+    stack.pop_back();
+
+    result.push_back(node->value);
+
+    if (node->right != nullptr) {
+      stack.push_back(node->right);
+    }
+
+    if (node->left != nullptr) {
+      stack.push_back(node->left);
+    }
+
+  } while (!stack.empty());
+
+  return result;
+}
 
 #endif
