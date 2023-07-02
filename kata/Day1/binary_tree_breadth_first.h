b9125ddbceec6e83b5815d1ca104b3a6ec8a0f60
Scott-fo <scottfo@live.com>
2023-07-02 22:06:19 +0100

    feat: day1 breadth first search bt

--- kata/Day1/binary_tree_breadth_first.h
+++ kata/Day1/binary_tree_breadth_first.h
@@ -2,10 +2,36 @@
 #define BINARY_TREE_BREADTH_FIRST_H
 
 #include "types.h"
+#include <queue>
 #include <vector>
 
 using std::vector;
 
-inline vector<int> breadth_first(BinaryNode<int> *head) {}
+inline vector<int> breadth_first(BinaryNode<int> *head) {
+  vector<int> result;
+  std::queue<BinaryNode<int> *> queue;
+
+  if (head == nullptr) {
+    return result;
+  }
+
+  queue.push(head);
+  do {
+    BinaryNode<int> *node = queue.front();
+    queue.pop();
+    result.push_back(node->value);
+
+    if (node->left != nullptr) {
+      queue.push(node->left);
+    }
+
+    if (node->right != nullptr) {
+      queue.push(node->right);
+    }
+
+  } while (!queue.empty());
+
+  return result;
+}
 
 #endif
