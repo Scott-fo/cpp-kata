ba7894f18ae622d6633f60ab8153f587cfdf4599
Scott-fo <scottfo@live.com>
2023-07-03 15:01:44 +0100

    feat: post order

--- kata/Day1/binary_tree_post_order.h
+++ kata/Day1/binary_tree_post_order.h
@@ -7,6 +7,27 @@
 
 using std::vector;
 
-inline vector<int> post_order(BinaryNode<int> *head) {}
+inline void post_order_helper(BinaryNode<int> *node, vector<int> *result) {
+  if (node == nullptr) {
+    return;
+  }
+
+  if (node->left != nullptr) {
+    post_order_helper(node->left, result);
+  }
+
+  if (node->right != nullptr) {
+    post_order_helper(node->right, result);
+  }
+
+  result->push_back(node->value);
+}
+
+inline vector<int> post_order(BinaryNode<int> *head) {
+  vector<int> result;
+  post_order_helper(head, &result);
+
+  return result;
+}
 
 #endif
