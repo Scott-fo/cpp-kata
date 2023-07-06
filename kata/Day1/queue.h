dc43a22e73e86e8299d55d64c64e96e4b9fdfe52
Scott-fo <scottfo@live.com>
2023-07-06 14:08:54 +0100

    feat: stack, queue, remove map

--- kata/Day1/queue.h
+++ kata/Day1/queue.h
@@ -1,15 +1,66 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdexcept>
+
+template <typename T> struct Node {
+  T value;
+  Node<T> *next;
+
+  Node(T val) : value(val) { next = nullptr; }
+};
+
 template <typename T> class Queue {
 public:
-  Queue() {}
+  Queue() : length(0) { head = tail = nullptr; }
 
-  void enqueue(T item) {}
+  void enqueue(T item) {
+    Node<T> *node = new Node<T>(item);
 
-  T dequeue() {}
+    length++;
+    if (tail == nullptr) {
+      head = tail = node;
+      return;
+    }
 
-  T peek() {}
+    tail->next = node;
+    tail = node;
+  }
+
+  T dequeue() {
+    if (head == nullptr) {
+      throw std::underflow_error("Length of queue is 0");
+    }
+
+    Node<T> *node = head;
+    T value = node->value;
+
+    head = head->next;
+
+    length--;
+    delete node;
+
+    if (length == 0) {
+      tail = nullptr;
+    }
+
+    return value;
+  }
+
+  T peek() {
+    if (head == nullptr) {
+      throw std::underflow_error("Length of queue is 0");
+    }
+
+    return head->value;
+  }
+
+  int size() { return length; }
+
+private:
+  int length;
+  Node<T> *head;
+  Node<T> *tail;
 };
 
 #endif
--- kata/Day1/stack.h
+++ kata/Day1/stack.h
@@ -1,18 +1,54 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdexcept>
+
+template <typename T> struct Node {
+  T value;
+  Node<T> *next;
+
+  Node(T val) : value(val) { next = nullptr; }
+};
+
 template <typename T> class Stack {
 public:
-  Stack() {}
+  Stack() : length(0) { head = nullptr; }
 
-  void push(T item) {}
+  void push(T item) {
+    Node<T> *node = new Node<T>(item);
+    node->next = head;
+    head = node;
+    length++;
+  }
 
-  T pop() {}
+  T pop() {
+    if (length == 0) {
+      throw std::underflow_error("Length is 0");
+    }
 
-  T peek() {}
+    Node<T> *node = head;
+    T value = node->value;
 
-public:
+    head = head->next;
+
+    delete node;
+
+    length--;
+
+    return value;
+  }
+
+  T peek() {
+    if (length == 0) {
+      throw std::underflow_error("Length is 0");
+    }
+
+    return head->value;
+  }
+
+private:
   int length;
+  Node<T> *head;
 };
 
 #endif
